a55394b20fe24522bdfedb0c8730417d74089ce9
shirry0129 <shimizy35@gmail.com>
2017-07-28 09:48:18 +0900

    add int2bit.c

---
+++ int2bit.c
@@ -0,0 +1,35 @@
+//int型変数の内容を16進/2進表示する
+
+#include<stdio.h>
+
+void printbit(unsigned char *_p){
+	int i;
+	int binary[8];
+
+	unsigned char p=*_p;
+	for(i=7;i>=0;i--){
+		binary[i]=p%2;
+		p=p/2;
+	}
+	for(i=0;i<8;i++){
+		printf("%d",binary[i]);
+	}
+
+	putchar(' ');
+}
+
+int main(int argc,char **argv){
+	int x=-123456;	// -123456 = 0xfffe1dc0
+	unsigned char *p;
+	int i;
+
+	p=(unsigned char *)&x;
+	printf("%d\n",x);
+	printf("%02x %02x %02x %02x\n",*p,*(p+1),*(p+2),*(p+3));
+	for(i=0;i<4;i++){
+		printbit(p+i);
+	}
+	putchar('\n');
+
+	return 0;
+}
